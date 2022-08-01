5cac9d0ab33f923bc174e9fa9f52d9981673a858
Edward Zhang <643431636@qq.com>
2022-08-01 19:44:11 +0800

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,54 +3,58 @@ const fs = require('fs');
 
 module.exports = {
   entry: './src/index.js',
-  
+
   output: {
     filename: 'js/index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
 
   devServer: {
     inline: true,
     contentBase: path.resolve(__dirname, 'dist'),
     port: 7070,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  
+
+  mode:"production",
+
   module: {
     rules: [
-        {
-          test: /\.html$/,
-          use: ["file?name=[name].[ext]"]
-        },
+      {
+        test: /\.html$/,
+        use: ['file?name=[name].[ext]'],
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         resolve: {
-          extensions: ['.js', '.jsx']
+          extensions: ['.js', '.jsx'],
         },
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-            outputPath: 'assets',
-        }
+          outputPath: 'assets',
+        },
       },
       {
-        test: /\.(jpe?g|png|gif)$/i, 
-        loader: "file-loader?name=/img/[name].[ext]"
-      }
-    ]
-  }
-
+        test: /\.(jpe?g|png|gif)$/i,
+        loader: 'file-loader',
+        options:{
+          name:"/img/[name].[ext]"
+        }
+      },
+    ],
+  },
 };
